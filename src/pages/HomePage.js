894571f1af20958d789ae9cff3840d108f495895
吴冬辰 <dong-chen.wu@utsoft.cn>
2017-01-11 14:11:19 +0800

    BestKeep init
    BestKeep init

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -152,7 +152,7 @@ export default class HomePage extends Component {
     }
 
     getHomeData() {
-        fetch("http://api.test.bestkeep.cn/index_v228").then((response)=>response.json())
+        fetch("http://www.baidu.com").then((response)=>response.json())
             .then((responseJson)=> {
                 let result = responseJson.data;
                 this.setState({
